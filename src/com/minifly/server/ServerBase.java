9d416465da33bd10cf560000be1d5777c4772787
贺晓飞 <hexiaofei@iscs.com.cn>
2017-02-22 19:32:49 +0800

    目录结构没有提交吧。

---
+++ src/com/minifly/server/ServerBase.java
@@ -0,0 +1,8 @@
+package com.minifly.server;
+
+public class ServerBase {
+	public static void main(String arg[]){
+		int result = (int)(((2 + 41*(1f)))  / (60)*100);
+		System.out.print(""+result);
+	}
+}
