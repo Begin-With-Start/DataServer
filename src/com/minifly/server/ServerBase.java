4669176d7ae96f40ee7ec5e450718d074594933e
贺晓飞 <hexiaofei@iscs.com.cn>
2017-02-22 11:16:02 +0800

    Revert "第一次提交。"
    This reverts commit 649071fdcb9c9baa78ead347794408826d6a9c8f.

--- src/com/minifly/server/ServerBase.java
+++
@@ -1,8 +0,0 @@
-package com.minifly.server;
-
-public class ServerBase {
-	public static void main(String arg[]){
-		int result = (int)(((2 + 41*(1f)))  / (60)*100);
-		System.out.print(""+result);
-	}
-}
